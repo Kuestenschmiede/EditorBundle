2c8de858cdbba924005d156412874792ea9b52eb
coastforge_mei <matthias.eilers@kuestenschmiede.de>
2020-02-13 21:31:27 +0100

    add px hint

--- Resources/contao/languages/de/tl_c4g_map_locstyles.php
+++ Resources/contao/languages/de/tl_c4g_map_locstyles.php
@@ -14,7 +14,7 @@
 
 
 $GLOBALS['TL_LANG']['tl_c4g_map_locstyles']['editor_icon'] 	 = array('Icon im Editor','Das Icon wird im Editor an Stelle des Standardicons verwendet.');
-$GLOBALS['TL_LANG']['tl_c4g_map_locstyles']['editor_icon_size'] = array('Größe des Editoricons','Geben Sie die gewünschte Icongröße an.');
+$GLOBALS['TL_LANG']['tl_c4g_map_locstyles']['editor_icon_size'] = array('Größe des Editoricons in Pixeln','Geben Sie die gewünschte Icongröße an.');
 $GLOBALS['TL_LANG']['tl_c4g_map_locstyles']['editor_sort']      = array('Sortierung','Reihenfolge im Editor für diesen Lokationsstil.');
 $GLOBALS['TL_LANG']['tl_c4g_map_locstyles']['editor_vars'] 	 = array('Zusatzfelder','Zusatzfelder stehen im Editor zur Verfügung und können über ${Schlüssel} im Popup, Label und Tooltip in die Karte eingefügt werden.');
 $GLOBALS['TL_LANG']['tl_c4g_map_locstyles']['editor_collect'] 	 = array('Geometrien zusammenfassen','Fasst alle Geometrien dieses Lokationsstils, die miteinander im Editor bearbeitet werden, zusammen.');
